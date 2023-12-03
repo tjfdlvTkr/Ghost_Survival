a5115fac09ecf97f2bf31c317a199c7c81e112dd
tjfdlvTkr <junha212@gmail.com>
2023-12-04 04:26:23 +0900

    Update: Monster

--- Assets/_Scripts/Monster.cs
+++ Assets/_Scripts/Monster.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Monster : MonoBehaviour
 {
     public GameObject player;
@@ -9,6 +10,66 @@ public class Monster : MonoBehaviour
 
     private float detectionRange = 5f;
     private float attackRange = 2f;
+
+    public float speed;
+
+    public float health;
+    public float maxHealth;
+    public RuntimeAnimatorController[] animCon;
+    public Rigidbody2D target;
+
+    [System.Serializable]
+    public class SpawnData
+    {
+        public int spriteType;
+        public float speed = 10f;
+        public float health = 50;
+        // 필요한 다른 데이터들을 추가로 정의할 수 있습니다.
+    }
+
+    bool isLive;
+
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriter;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isLive)
+        {
+            return;
+        }
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        if (!isLive)
+        {
+            return;
+        }
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
+
+   
+    public void Init(SpawnData data)
+    {
+        anim.runtimeAnimatorController = animCon[data.spriteType];
+        speed = data.speed;
+        maxHealth = data.health;
+        health = data.health;
+    }
 
     private void Start()
     {
@@ -21,22 +82,39 @@ public class Monster : MonoBehaviour
 
         if (distance <= attackRange)
         {
-            // 거리가 2칸 이하로 가까워지면 공격 애니메이션을 재생합니다.
             animator.SetBool("Attack", true);
             animator.SetBool("Run", false);
         }
         else if (distance <= detectionRange)
         {
-            // 거리가 5칸 이하로 가까워지면 달리기 애니메이션을 재생합니다.
             animator.SetBool("Run", true);
             animator.SetBool("Attack", false);
         }
         else
         {
-            // 그 외의 경우에는 모든 애니메이션을 중지합니다.
             animator.SetBool("Run", false);
             animator.SetBool("Attack", false);
         }
     }
-}
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Weapon"))
+            return;
+
+        health -= collision.GetComponent<Weapon>().damage;
+
+        if (health > 0)
+        {
+            // 몬스터의 피격 처리 로직을 추가해야 합니다.
+        }
+        else
+        {
+            Dead();
+        }
+    }
+    void Dead()
+    {
+        gameObject.SetActive(false);
+    }
+}
